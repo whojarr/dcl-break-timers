28aaf1674306c27bff1db2f2c7e2a4d58d210295
David Hunter <davidhunter@catalyst.net.nz>
2022-09-27 23:43:54 +1300

    add push messages from timer to zoom chat add zoom meetings and meeting info pages via api added zoom.py to call zoom apis for data

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import os
+import json
 from urllib import response
 from flask import Flask
-from flask import render_template
-from pyzoom import ZoomClient
+from flask import render_template, request
+import zoom
+from dotenv import load_dotenv
 
+load_dotenv()
+
+ZOOM_ACCOUNT_ID = os.getenv('ZOOM_ACCOUNT_ID')
+ZOOM_CLIENT_ID = os.getenv('ZOOM_CLIENT_ID')
+ZOOM_CLIENT_SECRET = os.getenv('ZOOM_CLIENT_SECRET')
+
+zoom_client = zoom.Zoom(ZOOM_ACCOUNT_ID, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET)
 
 template_dir = os.path.abspath('./')
 app = Flask(__name__, template_folder=template_dir)
+
+
+def zoom_chat_send(meeting_id, message):
+
+    meeting_id = 'f787566257a44d798e05c8407f627882'
+
+    return zoom_client.chat_send(meeting_id, message)
+
+
 
 @app.route("/")
 def index_html():
@@ -29,12 +47,67 @@ def dates_html():
 def timer_html():
     return render_template("timer.html", **locals())
 
+@app.route("/zoom.html")
+def zoom_html():
+    return render_template("zoom.html", **locals())
 
-def zoom_me():
 
-    zoom_client = ZoomClient('SI6hJxlaTYeUlbUcFmwu9Q', 'QmLnnClzUZ6vRmKcmgi3Alznd7w6x8Od')
-    response = zoom_client.meetings.list_meetings()
-    print(response)
+@app.route("/zoom_meeting_info.html")
+def zoom_meeting_info_html():
+    return render_template("zoom_meeting_info.html", **locals())
+
+
+@app.route("/api/chat/send", methods = ['GET','POST'])
+def chat_send():
+
+    if request.method == 'GET':
+        message = request.values.get("message")
+    if request.method == 'POST':
+        message = request.json['message']
+
+    zoom_response = zoom_chat_send(message)
+    resp = app.response_class(
+        response=json.dumps(zoom_response),
+        status=200,
+        mimetype='application/json'
+    )
+    return resp
+
+
+@app.route("/api/zoom/meeting", methods = ['GET'])
+def zoom_meeting():
+
+    if request.method == 'GET':
+
+        meeting_id = request.values.get("meeting_id")
+
+        zoom_response = zoom_client.meeting(meeting_id)
+
+        resp = app.response_class(
+            response=json.dumps(zoom_response),
+            status=200,
+            mimetype='application/json'
+        )
+
+    return resp
+
+
+@app.route("/api/zoom/meetings", methods = ['GET'])
+def zoom_meetings():
+
+    if request.method == 'GET':
+
+        type = request.values.get("type")
+
+        zoom_response = zoom_client.meeting_list(type)
+
+        resp = app.response_class(
+            response=json.dumps(zoom_response),
+            status=200,
+            mimetype='application/json'
+        )
+
+    return resp
 
 
 
---
+++ zoom.py
@@ -0,0 +1,170 @@
+from asyncio import format_helpers
+import datetime
+import json
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class Zoom():
+
+    def __init__(self, account_id, client_id, client_secret):
+
+        self.account_id = account_id
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token_expires = None
+        self.access_token_data = ""
+
+
+    def chat_channels(self):
+
+        url = "https://api.zoom.us/v2/chat/users/me/channels/"
+        headers = {
+            "Content-Type": "Application/json",
+            "Authorization": f"Bearer {self.token()}"
+        }
+
+        response = requests.get(url, headers=headers)
+
+        return(response.json())
+
+
+    def chat_send(self, channel_id=None, message="Hello"):
+
+        url = f"https://api.zoom.us/v2/chat/users/me/messages"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token()}"
+        }
+        data = {
+            "to_channel": channel_id,
+            "message": message
+        }
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+
+        return response.json()
+
+
+    def meeting(self,meeting_id=None):
+
+        if meeting_id:
+            url = f"https://api.zoom.us/v2/meetings/{meeting_id}"
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.token()}"
+            }
+            response = requests.get(url, headers=headers)
+
+            return response.json()
+
+
+    def meeting_list(self,type=""):
+
+        url = f"https://api.zoom.us/v2/users/me/meetings/?type={type}"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token()}"
+        }
+        response = requests.get(url, headers=headers)
+
+        return response.json()
+
+
+    def meeting_metrics(self,meeting_id=None):
+
+        if meeting_id:
+
+            url = f"https://api.zoom.us/v2/metrics/meetings/{meeting_id}"
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.token()}"
+            }
+            response = requests.get(url, headers=headers)
+
+            return response.json()
+ 
+
+    def meeting_messages(self, meeting_id = None):
+
+        if not meeting_id:
+            meeting_id = self.meeting_pmi()
+
+        url = f"https://api.zoom.us/v2/live_meetings/{meeting_id}/chat/messages/"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token()}"
+        }
+        response = requests.get(url, headers=headers)
+
+        return response.json()
+
+
+    def meeting_pmi(self):
+
+        url = f"https://api.zoom.us/v2/users/me"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token()}"
+        }
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+
+            return response.json()['pmi']
+
+        else:
+
+            return response.status_code
+
+
+    def myconverter(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.__str__()
+
+
+    def token(self):
+
+        if self.token_expired():
+            self.token_refresh()
+
+        return self.access_token_data['access_token']
+
+
+    def token_data_load(self):
+        with open('auth_token.tmp', 'r') as file:
+            raw = file.read()
+            data = json.loads(raw)
+
+            expires = data['expires_date']
+            self.access_token_expires = datetime.datetime.strptime(expires, "%Y-%m-%d %H:%M:%S.%f")
+            self.access_token_data = data
+
+
+    def token_expired(self):
+        if not self.access_token_expires:
+            if os.path.isfile('auth_token.tmp'):
+                self.token_data_load()
+            else:
+                print('token expired, refreshing token')
+                self.token_refresh()
+
+        if self.access_token_expires < datetime.datetime.now():
+            return True
+        return False
+
+
+    def token_refresh(self):
+        url = f'https://zoom.us/oauth/token?grant_type=account_credentials&account_id={self.account_id}'
+        response = requests.post(url, auth=HTTPBasicAuth(self.client_id,self.client_secret))
+        self.access_token_data = response.json()
+        self.access_token_expires = datetime.datetime.now() + datetime.timedelta(seconds=self.access_token_data['expires_in'])
+        self.token_store()
+
+
+    def token_store(self):
+        with open('auth_token.tmp', 'w') as file:
+            store_data = self.access_token_data
+            store_data["expires_date"] = self.access_token_expires
+
+            file.write(json.dumps(store_data, default=self.myconverter, indent=4))
